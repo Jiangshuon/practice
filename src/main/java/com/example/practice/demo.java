733f745347c6480dceba472cd2cc109261627fb2
admin <11>
2021-09-06 10:16:51 +0800

    2

--- src/main/java/com/example/practice/demo.java
+++ src/main/java/com/example/practice/demo.java
@@ -5,6 +5,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
-        System.out.println("test1");
+        System.out.println("test2");
     }
 }
