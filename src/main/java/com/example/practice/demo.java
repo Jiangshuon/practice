022a31272f3dc4d1b2f964f6c0b6e7ef0ebc80d5
admin <11>
2021-09-06 10:13:23 +0800

    first

---
+++ src/main/java/com/example/practice/demo.java
@@ -0,0 +1,10 @@
+package com.example.practice;
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test1");
+    }
+}
